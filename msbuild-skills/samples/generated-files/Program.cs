9b68602a02fac79ecd9704735f06572af410f81e
Jan Krivanek <jankrivanek@microsoft.com>
2026-02-17 18:37:32 +0100

    Adjust bin-obj sample

--- msbuild-skills/samples/generated-files/Program.cs
+++ msbuild-skills/samples/generated-files/Program.cs
@@ -1,11 +1,9 @@
-namespace GeneratedFiles;
+using TestProject.Generated;
 
-public class App
-{
-    public static void Main()
-    {
-        // This will fail with CS0103 because VersionInfo is generated
-        // but not included in the Compile items
-        System.Console.WriteLine(VersionInfo.Version);
-    }
-}
+Console.WriteLine("=== Build Information (from generated code) ===");
+Console.WriteLine($"Project Name:     {BuildInfo.ProjectName}");
+Console.WriteLine($"Build Time:       {BuildInfo.BuildTime}");
+Console.WriteLine($"Machine Name:     {BuildInfo.MachineName}");
+Console.WriteLine($"Configuration:    {BuildInfo.Configuration}");
+Console.WriteLine($"Target Framework: {BuildInfo.TargetFramework}");
+Console.WriteLine("================================================");
